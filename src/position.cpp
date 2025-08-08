2823a65fec015c3bfb380a3e4744469cdcd2660f
Abdullah <iamabdullah.dev@gmail.com>
2025-08-08 10:46:04 +0500

    F1

--- src/position.cpp
+++ src/position.cpp
@@ -298,19 +298,32 @@ Position& Position::set(const string& fenStr, bool isChess960, StateInfo* si, Th
   // White gating pieces in XBoard format
   if (xboard)
   {
-      Square s1 = SQ_A1, s2 = SQ_A1;
+      Square gatingPositions[MAX_GATES];
+      std::fill_n(gatingPositions, MAX_GATES, SQ_NONE);
+      
       for (Square s = SQ_A1; (ss >> token) && !isspace(token) && s <= SQ_H1; s++)
       {
           if ((idx = PieceToChar.find(token)) != string::npos)
           {
-              if (type_of(Piece(idx)) == gating_piece(GATE_1))
-                  s1 = s;
-              else
-                  s2 = s;
+              PieceType pt = type_of(Piece(idx));
+              // Find which gate this piece type corresponds to
+              for (Gate g = GATE_1; g < GATE_NB; ++g)
+              {
+                  if (pt == gating_piece(g))
+                  {
+                      gatingPositions[g - 1] = s;  // g-1 because GATE_1 is index 1
+                      break;
+                  }
+              }
           }
       }
-      put_gating_piece(WHITE, s1);
-      put_gating_piece(WHITE, s2);
+      
+      // Place all found gating pieces
+      for (int i = 0; i < MAX_GATES; ++i)
+      {
+          if (gatingPositions[i] != SQ_NONE)
+              put_gating_piece(WHITE, gatingPositions[i]);
+      }
   }
   // Gating pieces
   else if (token == '[')
